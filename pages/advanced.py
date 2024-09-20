f723484bcece274535300e3244467f169601655f
Ross Cohen <rosscohen1990@gmail.com>
2024-09-20 18:07:58 -0400

    Update advanced.py

--- pages/advanced.py
+++ pages/advanced.py
@@ -41,7 +41,7 @@ ensure_states(states)
 # 3: Define Streamlit page structure and functionality
 st.write(
     """
-# Infinite Bookshelf: Write full books using llama3 (8b and 70b) on Groq
+# Bookify Advanced: Write full books using the power of AI Tutor - in advanced mode
 """
 )
 
